e81c9bba7f106bcfa78feb6e0b7f1432011d6c37
Chirag Jain <masterchi20@icloud.com>
2021-09-06 22:04:33 +0100

    Corrected logic for object detection.

--- mapping.py
+++ mapping.py
@@ -1,6 +1,23 @@
+import picar_4wd as fc
+
 # Helper function which returns true if obstacle is within x cm
 def isAnythingInFront():
-    pass
+
+    # Using implementation from obstacle_avoidance.py in examples
+    # scan_list = fc.scan_step(35)
+    # if not scan_list:
+    #     return False
+
+    # tmp = scan_list[3:7]
+    # return tmp != [2,2,2,2]
+
+    # Don't turn servo, just return True when distance is 50 cm
+    threshhold = 50
+    distance = fc.get_distance_at(0)
+    if distance > threshhold or distance == -2:
+        return False
+    else:
+        return True
 
 # Helper function which turns to a random angle
 def stopAndTurnRandom():
