f6606e6110c1445f33ba045ea8d32b9beccf9fa2
Chirag <masterchi20@icloud.com>
2021-09-11 22:28:41 +0100

    Writing code to capture an image to be classified.

--- Part2/detect_object.py
+++ Part2/detect_object.py
@@ -9,3 +9,26 @@
 # 7. Not defined
 # 8. Not defined
 # 9. Current car position
+
+from io import BytesIO
+from time import sleep
+from picamera import PiCamera
+from PIL import Image
+
+class Eye:
+
+    def __init__(self):
+        self.stream = BytesIO()
+        self.camera = PiCamera()
+        self.camera.start_preview()
+        self.image = None
+
+    def captureImage(self):
+        self.camera.capture(self.stream, format='jpeg')
+        # "Rewind" the stream to the beginning so we can read its content
+        self.stream.seek(0)
+        self.image = Image.open(self.stream)
+
+    def classifyImage(self):
+        self.image.show()
+
