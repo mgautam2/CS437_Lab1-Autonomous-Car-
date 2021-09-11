a89a9c02bf8e309c86b94d3b4e58a30def953ff2
Chirag <masterchi20@icloud.com>
2021-09-11 22:06:52 +0100

    Created logic flow in main

---
+++ Part2/main.py
@@ -0,0 +1,32 @@
+import time
+import sys
+import signal
+
+import picar_4wd as fc
+import mapping as mp
+import detect_object as do
+import routing as rt
+
+def signal_handler(sig, frame):
+    fc.stop()
+    sys.exit(0)
+
+if __name__ == "__main__":
+
+    # Initialise routing parameters
+
+    # Run while loop
+    while True:
+
+        # Stop and build relative map
+
+        # Use A star to get to best possible location on current map
+
+        while True:
+
+            # Move to that location
+
+            # While driving, keep getting ultrasonic sensor data and 
+            # if an object exists in front, use openCV to detect object
+            pass
+
