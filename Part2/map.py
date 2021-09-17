2466dba3166bc6b3c336ce281a22ca19ae3af106
Grace Robbins <gcrobbins2@gmail.com>
2021-09-17 19:33:54 +0100

    minor code style changes

--- Part2/map.py
+++ Part2/map.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from constants import *
 
 class Map:
     
@@ -14,24 +15,24 @@ class Map:
         ultrasonic_map_width = ultrasonicMap.shape[1]
         for i in range(ultrasonicMap.shape[0]):
             for j in range(ultrasonic_map_width):
-                if (self.orientation == 0):
+                if (self.orientation == UP):
                     map_i_idx = self.current_position[0] + i
                     map_j_idx = self.current_position[1] + (j - math.floor(ultrasonic_map_width/2))
-                elif (self.orientation == 1): # facing 'right'
+                elif (self.orientation == LEFT):
                     map_i_idx = self.current_position[0] + (j - math.floor(ultrasonic_map_width/2))
                     map_j_idx = self.current_position[1] - i
-                elif (self.orientation == 2): # facing 'down'
+                elif (self.orientation == DOWN):
                     map_i_idx = self.current_position[0] - i
                     map_j_idx = self.current_position[1] - (j - math.floor(ultrasonic_map_width/2))
-                elif (self.orientation == 3): # facing 'left'
+                elif (self.orientation == RIGHT):
                     map_i_idx = self.current_position[0] - (j - math.floor(ultrasonic_map_width/2))
                     map_j_idx = self.current_position[1] + i
                 else:
                     print("ERROR: UNKNWON ORIENTATION IN MAP.PY:", self.orientation)
                     map_i_idx = 0
                     map_j_idx = 0
-                if (map_i_idx >= 0 and map_i_idx < self.map.shape[0] and map_j_idx >= 0 and map_j_idx < self.map.shape[1]):
-                    self.map[map_i_idx][map_j_idx] = ultrasonicMap[i][j]
+                if (self.isPointInBounds((map_j_idx, map_i_idx))):
+                    self.setLabelAtPoint((map_i_idx, map_j_idx), ultrasonicMap[i][j])
 
 
     def printSelf(self):
